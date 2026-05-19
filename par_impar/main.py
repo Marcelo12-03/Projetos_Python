7b7e931e62ff151ff9f68d1db8332ba74ebcf6d0
Marcelo12-03 <marcelo.baterry@gmail.com>
2026-05-19 08:51:02 -0300

    Add files via upload

---
+++ par_impar/main.py
@@ -0,0 +1,10 @@
+numero = int (input("Digite um numero: "))
+
+if numero % 2 ==0 and numero != 0:
+    print("Esse numero é par.")
+
+elif numero % 2 != 0:
+    print("Esse numero é impar")
+
+else:
+    print("Esse numero é igual a zero")
