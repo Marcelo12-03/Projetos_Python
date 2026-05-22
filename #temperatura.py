3af368e2c1c659a12562f7845a83237ebd45679e
Marcelo12-03 <marcelo.baterry@gmail.com>
2026-05-22 08:15:34 -0300

    Add files via upload

---
+++ #temperatura.py
@@ -0,0 +1,12 @@
+#temperatura.py
+
+temperatura = float(input("Digite a temperatura: "))
+
+if temperatura < 15:
+    print("Esta fria")
+
+elif temperatura <= 25:
+    print("Esta agradável")
+
+else:
+    print("Esta quente")
